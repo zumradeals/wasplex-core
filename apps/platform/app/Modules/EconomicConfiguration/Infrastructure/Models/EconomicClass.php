dec4e6d1ac1c1b7e6a4d3edd5028956877812d4f
zumradeals <zumradeals@gmail.com>
2026-08-03 20:56:20 +0000

    fix(economy): document economic class model types

--- apps/platform/app/Modules/EconomicConfiguration/Infrastructure/Models/EconomicClass.php
+++ apps/platform/app/Modules/EconomicConfiguration/Infrastructure/Models/EconomicClass.php
@@ -6,12 +6,18 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @property string $id
+ * @property string $code
+ * @property string $status
+ */
 final class EconomicClass extends Model
 {
     use HasUuids;
 
     protected $guarded = [];
 
+    /** @return HasMany<EconomicClassVersion, $this> */
     public function versions(): HasMany
     {
         return $this->hasMany(EconomicClassVersion::class);
