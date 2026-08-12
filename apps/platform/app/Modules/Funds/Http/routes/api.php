ceca23d139571d8e1a6ed2790595eb0a404aa27e
zumradeals <zumradeals@gmail.com>
2026-08-12 22:41:25 +0000

    feat(funds): exposer routes devis admin et pro

--- apps/platform/app/Modules/Funds/Http/routes/api.php
+++ apps/platform/app/Modules/Funds/Http/routes/api.php
@@ -2,8 +2,11 @@
 
 declare(strict_types=1);
 
+use App\Modules\Funds\Http\Controllers\Admin\AdminFundQuotesController;
 use App\Modules\Funds\Http\Controllers\Admin\AdminFundsController;
+use App\Modules\Funds\Http\Controllers\Professional\ProfessionalFundQuotesController;
 use App\Modules\Funds\Http\Controllers\User\FundsController;
+use App\Modules\Identity\Http\Middleware\EnsureActiveProfessionalOrganization;
 use App\Modules\Identity\Http\Middleware\EnsureCapability;
 use App\Modules\Identity\Http\Middleware\EnsureRecentMfa;
 use App\Modules\Identity\Http\Middleware\EnsureSessionNotRevoked;
@@ -19,6 +22,17 @@ Route::middleware(['auth', EnsureSessionNotRevoked::class])
         Route::post('/wishes', [FundsController::class, 'storeWish']);
         Route::post('/wishes/{wish}/submit', [FundsController::class, 'submitWish']);
         Route::post('/wishes/{wish}/contributions', [FundsController::class, 'contributeWish']);
+    });
+
+Route::middleware(['auth', EnsureSessionNotRevoked::class, EnsureActiveProfessionalOrganization::class])
+    ->prefix('professional/funds')
+    ->group(function (): void {
+        Route::get('/quote-requests', [ProfessionalFundQuotesController::class, 'index'])
+            ->middleware(EnsureCapability::class.':professional.funds.quote.view,organization:professional_organization_id');
+        Route::post('/quote-requests/{quoteRequest}/quote', [ProfessionalFundQuotesController::class, 'submit'])
+            ->middleware(EnsureCapability::class.':professional.funds.quote.respond,organization:professional_organization_id');
+        Route::post('/quote-requests/{quoteRequest}/decline', [ProfessionalFundQuotesController::class, 'decline'])
+            ->middleware(EnsureCapability::class.':professional.funds.quote.respond,organization:professional_organization_id');
     });
 
 Route::middleware(['auth', EnsureSessionNotRevoked::class, EnsureRecentMfa::class])
@@ -39,5 +53,12 @@ Route::middleware(['auth', EnsureSessionNotRevoked::class, EnsureRecentMfa::clas
         Route::patch('/categories/{category}', [AdminFundsController::class, 'updateCategory'])
             ->middleware(EnsureCapability::class.':admin.funds.manage');
         Route::post('/wishes/{wish}/review', [AdminFundsController::class, 'reviewWish'])
+            ->middleware(EnsureCapability::class.':admin.funds.review');
+
+        Route::get('/partners', [AdminFundQuotesController::class, 'partners'])
+            ->middleware(EnsureCapability::class.':admin.funds.view');
+        Route::post('/wishes/{wish}/quote-requests', [AdminFundQuotesController::class, 'requestQuotes'])
+            ->middleware(EnsureCapability::class.':admin.funds.review');
+        Route::post('/wishes/{wish}/quotes/{quote}/select', [AdminFundQuotesController::class, 'selectQuote'])
             ->middleware(EnsureCapability::class.':admin.funds.review');
     });
